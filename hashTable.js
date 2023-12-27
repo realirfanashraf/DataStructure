d9b438f4d6cae599fd7b226bafa3514c0e3fc90b
Irfan Ashraf <realirfanashraf@gmail.com>
2023-12-27 20:00:17 +0530

    handle collision

--- hashTable.js
+++ hashTable.js
@@ -7,30 +7,54 @@ class HashTable {
     hash(key) {
         let total = 0;
         for (let i = 0; i < key.length; i++) {
-            total += key.charCodeAt(i); 
+            total += key.charCodeAt(i);
         }
         return total % this.size;
     }
 
     set(key, value) {
         const index = this.hash(key);
-        this.table[index] = value;
+        let bucket = this.table[index];
+        if (!bucket) {
+            bucket = [[key, value]];
+            this.table[index] = bucket;
+        } else {
+            const sameItemKey = bucket.find(item => item[0] === key);
+            if (sameItemKey) {
+                sameItemKey[1] = value;
+            } else {
+                bucket.push([key, value]);
+            }
+        }
     }
 
     get(key) {
         const index = this.hash(key);
-        return this.table[index];
+        const bucket = this.table[index];
+        if (bucket) {
+            const sameItemKey = bucket.find(item => item[0] === key);
+            if (sameItemKey) {
+                return sameItemKey[1];
+            }
+        }
+        return undefined;
     }
 
     remove(key) {
         const index = this.hash(key);
-        this.table[index] = undefined;
+        const bucket = this.table[index];
+        if (bucket) {
+            const sameItemKey = bucket.find(item => item[0] === key);
+            if (sameItemKey) {
+                bucket.splice(bucket.indexOf(sameItemKey), 1);
+            }
+        }
     }
 
-    display(){
-        for(let i=0; i<this.table.length; i++){
-            if(this.table[i]){
-                console.log(i, this.table[i])
+    display() {
+        for (let i = 0; i < this.table.length; i++) {
+            if (this.table[i]) {
+                console.log(i, this.table[i]);
             }
         }
     }
