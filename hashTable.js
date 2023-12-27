26076c9fc9b53192db972171bb640fd8cdac8298
Irfan Ashraf <realirfanashraf@gmail.com>
2023-12-27 19:05:58 +0530

    hashtable

--- hashTable.js
+++ hashTable.js
@@ -1,14 +1,29 @@
-class HashTable{
-    constructor(size){
-        this.table = new Array(size)
-        this.size = size
+class HashTable {
+    constructor(size) {
+        this.table = new Array(size);
+        this.size = size;
     }
 
-    hash(key){
-        let total = 0
-        for(let i=0; i<key.length; i++){
-            total+=charCodeAt(i)
+    hash(key) {
+        let total = 0;
+        for (let i = 0; i < key.length; i++) {
+            total += key.charCodeAt(i); 
         }
-        return total % this.size
+        return total % this.size;
+    }
+
+    set(key, value) {
+        const index = this.hash(key);
+        this.table[index] = value;
+    }
+
+    get(key) {
+        const index = this.hash(key);
+        return this.table[index];
+    }
+
+    remove(key) {
+        const index = this.hash(key);
+        this.table[index] = undefined;
     }
 }
