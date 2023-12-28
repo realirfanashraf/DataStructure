d3f7a9a0f0f816cfa1665bdb9934026c4f574eac
Irfan Ashraf <realirfanashraf@gmail.com>
2023-12-28 19:35:11 +0530

    min and max

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -98,6 +98,23 @@ class BinarySearchTree{
             }
         }
     }
+
+    min(root) {
+        if (!root.left) {
+            return root.value;
+        } else {
+            return this.min(root.left);
+        }
+    }
+    
+    max(root) {
+        if (!root.right) {
+            return root.value;
+        } else {
+            return this.max(root.right);
+        }
+    }
+    
     
     
 }
