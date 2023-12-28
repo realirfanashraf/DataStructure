5618307f3e6a1a8e4fd01ff919f72a8066977d99
Irfan Ashraf <realirfanashraf@gmail.com>
2023-12-28 20:33:13 +0530

    delete

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -115,6 +115,34 @@ class BinarySearchTree{
         }
     }
     
+    delete(value) {
+        this.root = this.deleteNode(this.root, value);
+    }
     
+    deleteNode(root, value) {
+        if (root === null) {
+            return null;
+        }
+    
+        if (value < root.value) {
+            root.left = this.deleteNode(root.left, value);
+        } else if (value > root.value) {
+            root.right = this.deleteNode(root.right, value);
+        } else {
+            if (!root.left && !root.right) {
+                return null;
+            }
+            if (!root.left) {
+                return root.right;
+            } else if (!root.right) {
+                return root.left;
+            }
+    
+            root.value = this.min(root.right);
+            root.right = this.deleteNode(root.right, root.value);
+        }
+    
+        return root;
+    }    
     
 }
